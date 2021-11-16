729484e8ef7f889f7bbeef545e17685f1b396b92
ccqstark <ccqstark@qq.com>
2021-11-16 18:59:17 +0800

    No108将有序数组转换为二叉搜索树

---
+++ src/二叉树/No108将有序数组转换为二叉搜索树/SortedArrayToBST.java
@@ -0,0 +1,23 @@
+package 二叉树.No108将有序数组转换为二叉搜索树;
+
+import 二叉树.TreeNode;
+
+public class SortedArrayToBST {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBSTRecursion(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode sortedArrayToBSTRecursion(int[] nums, int left, int right) {
+        // 数组终点
+        if (left > right ){
+            return null;
+        }
+        // 取当前区间的作为当前root,这样写是为了防止超出数值范围（原来那种(left+right)/2会有这个问题）
+        int mid = left + ((right - left) / 2);
+        TreeNode root = new TreeNode(nums[mid]);
+        // 左右子树递归，范围都是左闭右闭
+        root.left = sortedArrayToBSTRecursion(nums,left, mid - 1);
+        root.right = sortedArrayToBSTRecursion(nums,mid + 1, right);
+        return root;
+    }
+}
