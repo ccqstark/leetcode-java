148b86effdbf4fe0354a30b4a9f82447e2be8a3c
ccqstark <ccqstark@qq.com>
2022-03-12 19:01:13 +0800

    No48旋转图像

---
+++ src/数组/No48旋转图像/Solution.java
@@ -0,0 +1,40 @@
+package 数组.No48旋转图像;
+
+public class Solution {
+
+    // 法1：不知道该怎么说，这些公式是一步步代入然后推导出来的，而且有点麻烦，感觉如果考到要么重新推导要么背诵下来。。。
+    public void rotate1(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+    // 法2（推荐）：先水平翻转（上下翻转），在沿主对角线翻转，即可实现旋转90度相同效果
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = tmp;
+            }
+        }
+        // 对角线翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+
+}
