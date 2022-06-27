c0946d22a932d6087160be5d35dd7316c5708dd4
ccqstark <ccqstark@qq.com>
2022-06-27 23:04:33 +0800

    补充注释：Floyd 判圈算法

--- src/数组/No287寻找重复数/Solution.java
+++ src/数组/No287寻找重复数/Solution.java
@@ -1,7 +1,8 @@
 package 数组.No287寻找重复数;
 
 /**
- * 这里只写了一种转为找有环链表的环入口的写法，其他写法较复杂，详细见官方题解
+ * 这里只写了一种转为找有环链表的环入口的写法(Floyd 判圈算法)
+ * 其他写法较复杂，详细见官方题解
  */
 public class Solution {
     public int findDuplicate(int[] nums) {
